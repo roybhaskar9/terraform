6b37945b24ff08619df923ce6e1db72e9fad85d2
roybhaskar9 <bhaskar@study9.com>
2018-12-28 21:07:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ resource "aws_security_group" "default"{
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress{
-        from_port = 80
-        to_port = 80
+        from_port = 8080
+        to_port = 8080
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
